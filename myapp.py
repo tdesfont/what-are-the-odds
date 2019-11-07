f9f74b5819a789779e88497680bf6bc9f9c7a5eb
tdesfont <desfontainesthibault@gmail.com>
2019-11-07 22:59:56 +0100

    Core of application is finished

--- myapp.py
+++ myapp.py
@@ -3,12 +3,15 @@ from flask import Flask
 from flask import g
 from flask import request, jsonify
 from flask import render_template, url_for, json
+from flask import flash
 from collections import defaultdict
 import solver
+import pandas as pd
+import os
 
 app = Flask(__name__)
 
-proba = None
+proba = 0
 
 # Loading universe.db
 def get_db(db_path):
@@ -40,24 +43,49 @@ def close_connection(exception):
     if db is not None:
         db.close()
 
+def convert_to_csv(graph):
+    planets = set()
+    sources = []
+    targets = []
+    weights = []
+
+    for source in graph:
+        planets.add(source)
+        for dest in graph[source]:
+            planets.add(dest)
+            sources.append(source)
+            targets.append(dest)
+            weights.append(graph[source][dest])
+
+    planets = list(planets)
+    d = {"id": planets}
+    df = pd.DataFrame(d)
+    df.to_csv('./static/data/nodes.csv', sep=",", index=False, header=True)
+
+    d = {"source": sources, "target": targets, "weight": weights}
+    df = pd.DataFrame(d)
+    df.to_csv('./static/data/edges.csv', sep=",", index=False, header=True)
+
+
 @app.route('/', methods=['GET', 'POST'])
 def index():
+    print(app.root_path)
     global proba
     # Load millenium-falcon json
     milleniumFalcon = json.load(open("millenium-falcon.json"))
     db_path = milleniumFalcon['routes_db']
     graph = graphFromDB("universe.db")
+    if not "nodes.csv" in os.listdir('./static/data/') or not "nodes.csv" in os.listdir('./static/data/'):
+        convert_to_csv(graph)
     print("\n Millenium Falcon: \n", milleniumFalcon)
     print("\n Graph: \n", graph)
 
     # Load empire intelligence json
     empire = request.get_json(silent=True)
     if empire:
-        print("\n Empire Json: \n", empire)
         proba = solver.odds(milleniumFalcon, graph, empire)
-        print("Survival proba:", proba)
-        return "The proba of survival is {}".format(proba)
-    return "Send a json using POST request to this page. Proba: {}".format(proba)
+
+    return render_template("home.html", percentage=proba*100)
 
 if __name__ == "__main__":
     app.run(debug=True)
--- solver.py
+++ solver.py
@@ -21,16 +21,63 @@ from helper_classes import Universe, Path
 optimalPath = None  # Best path available
 optimalBounty = +float("inf")  # Proba to be captured
 n_explored_path = 0
+stop_exploration = False
+
+def dag_explorer(path):
+
+    global optimalPath
+    global optimalBounty
+    global n_explored_path
+    global stop_exploration
+
+    # Stop all exploration in case optimal proba found of being captured is 0
+    if stop_exploration:
+        del path
+        return None
+
+    # Pruning
+    if path.is_out_of_fuel() or path.is_out_of_time() or path.n_days_bounty > optimalBounty:
+        n_explored_path += 1
+        del path
+        return None
+
+    # Checking Optimality Condition
+    if path.has_reached_destination() and path.n_days_bounty < optimalBounty:
+        optimalPath = copy.deepcopy(path)
+        optimalBounty = path.n_days_bounty
+        n_explored_path += 1
+        if path.n_days_bounty == 0:
+            stop_exploration = True
+        del path
+        return None
+
+    ###
+    # Continue recursive exploration
+    ###
+
+    # Option 1: Travel from the planet
+    available_destinations = path.get_destinations()
+    for destination in available_destinations:
+        new_path = copy.deepcopy(path)
+        new_path.go_to(destination)
+        dag_explorer(new_path)
+
+    # Option 2: Stay on the planet
+    path.wait()
+    dag_explorer(path)
+
 
 def odds(millenium_falcon, routes, empire):
 
     global optimalPath
     global optimalBounty
     global n_explored_path
+    global stop_exploration
 
     optimalPath = None
     optimalBounty = +float("inf")
     n_explored_path = 0
+    stop_exploration = False
 
     startNode = millenium_falcon['departure']
     targetNode = millenium_falcon['arrival']
@@ -50,40 +97,6 @@ def odds(millenium_falcon, routes, empire):
     n_explored_path = 0
 
     # Initial base case should be dag_explorer(Path(StartNode))
-    def dag_explorer(path):
-
-        global optimalPath
-        global optimalBounty
-        global n_explored_path
-
-        # Pruning
-        if path.is_out_of_fuel() or path.is_out_of_time() or path.n_days_bounty > optimalBounty:
-            n_explored_path += 1
-            del path
-            return None
-
-        # Checking Optimality Condition
-        if path.has_reached_destination() and path.n_days_bounty < optimalBounty:
-            optimalPath = copy.deepcopy(path)
-            optimalBounty = path.n_days_bounty
-            n_explored_path += 1
-            del path
-            return None
-
-        ###
-        # Continue recursive exploration
-        ###
-
-        # Option 1: Travel from the planet
-        available_destinations = path.get_destinations()
-        for destination in available_destinations:
-            new_path = copy.deepcopy(path)
-            new_path.go_to(destination)
-            dag_explorer(new_path)
-
-        # Option 2: Stay on the planet
-        path.wait()
-        dag_explorer(path)
 
     travel = Path(autonomy, countdown, startNode, targetNode, U)
     dag_explorer(travel)
